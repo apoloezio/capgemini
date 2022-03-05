ee3e01a5dd374efab53a985413fc1b24fe6b7fbc
apoloezio <100844599+apoloezio@users.noreply.github.com>
2022-03-05 17:57:36 -0300

    Add files via upload

---
+++ MedianaProjeto.java
@@ -0,0 +1,20 @@
+package mediana;
+
+public class MedianaProjeto {
+
+	int [] vet = new int [100]
+			//Mediana: 
+			
+		int esq=0;
+		int dir=vet.length-1;
+		int meio;
+		meio = (esq+dir)/2;
+		mediana = Vet[meio];
+	
+		}
+		
+		
+		// Mediana (1,2,4,6,9) = 4 (valor do meio)
+	}
+
+}
